ee029e8cca4d616e4152457c378f998414d56233
Clemens Fruhwirth <clemens@endorphin.org>
2019-02-07 16:33:57 +0100

    Move to segregated witness and add more tests.

--- basic_chain.py
+++ basic_chain.py
@@ -5,77 +5,70 @@ import hashlib
 import ed25519
 import collections
 
-class Input(object):
-    def __init__(self, tx_id, index, signature):
-        self.tx_id = tx_id
-        self.index = index
-        self.signature = signature
-        
-    def __repr__(self):
-        # The signature is not part of the representation
-        return "I(%s, %s)" % (self.tx_id, self.index)
-
+Input = collections.namedtuple("Input", ["tx_id", "index"])
 Output = collections.namedtuple("Output", ["pub_key", "amount"])
 
-class Transaction(object):
-    def  __init__(self, inputs, outputs):
-        self.inputs = inputs
-        self.outputs = outputs
 
-    def __repr__(self):
-        return "T(%s, %s)" % (self.inputs, self.outputs)
+class InputReferenceError(Exception):
+    pass
+
+
+class BadSignatureError(Exception):
+    pass
+
+
+class Transaction(collections.namedtuple("Transaction", ["inputs", "outputs"])):
     
     def tx_id(self):
         return hashlib.sha256(str(self)).digest()
 
-class TransactionBuilder(Transaction):
-    def __init__(self, inputs, outputs):
-        # bind all signatures to None in input.
-        # get tx_id
-        super(TransactionBuilder, self).__init__(inputs, outputs)
+    def make_witness(self, keys):
         tx_id = self.tx_id()
+        return [k.sign(tx_id) for k in keys]
 
-        for i in self.inputs:
-            i.signature = i.signature.sign(tx_id)
 
 class Chain(object):
+
     def __init__(self, genesis):
         # Do not verify the genesis transaction
         self.utxo = {}
         self.add_utxo(genesis)
 
     def add_utxo(self, tx):
-        i = 0
-        for o in tx.outputs:
-            self.utxo[(tx.tx_id(), i)] = o
-            i += 1 
-        
-    def process_tx(self, tx):
+        tx_id = tx.tx_id()
+        for o, idx in zip(tx.outputs, range(0, len(tx.outputs))):
+            self.utxo[(tx_id, idx)] = o
+
+    def process_tx(self, tx, witnesses):
         i_amount = 0
         tx_id = tx.tx_id()
-        effects = []
-        for i in tx.inputs:
+        new_utxo = dict(self.utxo)
+        for i, w in zip(tx.inputs, witnesses):
+            # Input reference unspent outputs in previous
+            # transactions. They do so with a tx_id and index
+            # reference.
             try:
-                o = self.utxo[(i.tx_id, i.index)]
+                o = new_utxo[(i.tx_id, i.index)]
+                del new_utxo[(i.tx_id, i.index)]
             except KeyError:
-                raise ValueError("Input %s not found in utxo set" % str(i))
-            
+                raise InputReferenceError("Input %s not found in utxo set" % str(i))
+
             try:
-                o.pub_key.verify(i.signature, tx_id)
+                # The witness must sign this tx_id with the pubkey in
+                # the output referenced.
+                o.pub_key.verify(w, tx_id)
             except ed25519.BadSignatureError:
-                raise ValueError("Invalid signature on input %s" % str(i))
-            def remove_utxo():
-                del self.utxo[(i.tx_id, i.index)]
-            effects += [remove_utxo]
+                raise BadSignatureError("Invalid signature on input %s" % str(i))
+
             i_amount += o.amount
 
         o_amount = sum([o.amount for o in tx.outputs])
         if i_amount < o_amount:
             raise ValueError("output amounts %d exceed input amounts %d", o_amount, i_amount)
 
-        # All checks passed. Modify our state
-        [e() for e in effects]
+        self.utxo = new_utxo
         self.add_utxo(tx)
+
 
 def new_key():
     return ed25519.create_keypair()
--- basic_chain_test.py
+++ basic_chain_test.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i python2 -p "with python2Packages; [python pysha3 ed25519]"
+#! nix-shell -i python2 -p "with python2Packages; [python ed25519]"
 
 from basic_chain import *
 import unittest
@@ -9,71 +9,83 @@ class TestFoo(unittest.TestCase):
         self.satoshi_priv, self.satoshi_pub = new_key()
         self.genesis_tx = Transaction([], [Output(pub_key=self.satoshi_pub, amount=1000)])
         self.c = Chain(self.genesis_tx)
-        
+        self.clemens_priv, self.clemens_pub = new_key()
+
+    def test_input_txid_error(self):
+        tx = Transaction([], [])
+        s_to_c = Transaction(
+            [Input(tx_id=tx.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=900),
+             Output(pub_key=self.clemens_pub, amount=100)])
+        with self.assertRaises(InputReferenceError):
+            self.c.process_tx(s_to_c, s_to_c.make_witness([self.satoshi_priv]))
+
     def test_input_index_error(self):
-        # Test invalid tx_id
-        # Test valid tx_id with invalid index
-        pass
-    
-    def test_double_spend(self):
-        # Check against double spends        
-        clemens_priv, clemens_pub = new_key()
-        s_to_c = TransactionBuilder([
-            Input(tx_id=self.genesis_tx.tx_id(),
-                  index=0,
-                  signature=self.satoshi_priv)], [
-            Output(pub_key=self.satoshi_pub,
-                   amount=900),
-            Output(pub_key=clemens_pub,
-                   amount=100)])
-        self.c.process_tx(s_to_c)
+        s_to_c = Transaction(
+            [Input(tx_id=self.genesis_tx.tx_id(), index=1)],
+            [Output(pub_key=self.satoshi_pub, amount=900),
+             Output(pub_key=self.clemens_pub, amount=100)])
+        with self.assertRaises(InputReferenceError):
+            self.c.process_tx(s_to_c, s_to_c.make_witness([self.satoshi_priv]))
+
+    def test_double_spend_tx(self):
+        """Tests double spending an output with two txs."""
+        s_to_c = Transaction(
+            [Input(tx_id=self.genesis_tx.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=900),
+             Output(pub_key=self.clemens_pub, amount=100)])
+        self.c.process_tx(s_to_c, s_to_c.make_witness([self.satoshi_priv]))
+        with self.assertRaises(InputReferenceError):
+            self.c.process_tx(s_to_c, s_to_c.make_witness([self.satoshi_priv]))
+
+    def test_double_spend_input(self):
+        """Tests double spending an output within the same tx by making it an input twice."""
+        tx = Transaction(
+            [Input(tx_id=self.genesis_tx.tx_id(), index=0),
+             Input(tx_id=self.genesis_tx.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=2000)])
+        with self.assertRaises(InputReferenceError):
+            self.c.process_tx(tx, tx.make_witness([self.satoshi_priv, self.satoshi_priv]))
+
+    def test_split_and_combine(self):
+        # tx1: Satoshi sends 100 coins to clemens, and takes 900 into a change output.
+        # tx2: Satoshi moves the 900 change output one more time.
+        # tx3: Clemens & Satoshi recombine 100 + 900 into the genesis like 1000.
+        s_to_c = Transaction(
+            [Input(tx_id=self.genesis_tx.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=900),
+             Output(pub_key=self.clemens_pub, amount=100)])
+        s_to_s = Transaction(
+            [Input(tx_id=s_to_c.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=900)])
+        self.c.process_tx(s_to_c, s_to_c.make_witness([self.satoshi_priv]))
 
         # Spend the new satoshi output
-        satoshi1 = TransactionBuilder([
-            Input(tx_id=s_to_c.tx_id(),
-                  index=0,
-                  signature=self.satoshi_priv)], [
-            Output(pub_key=self.satoshi_pub,
-                   amount=900)])
-        self.c.process_tx(satoshi1)
+        self.c.process_tx(s_to_s, s_to_s.make_witness([self.satoshi_priv]))
+        recombine = Transaction(
+            [Input(tx_id=s_to_c.tx_id(), index=1),
+             Input(tx_id=s_to_s.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=1000)])
+        self.c.process_tx(recombine, recombine.make_witness([self.clemens_priv, self.satoshi_priv]))
 
-        # Double spend new output
-        satoshi2 = TransactionBuilder([
-            Input(tx_id=s_to_c.tx_id(),
-                  index=0,
-                  signature=self.satoshi_priv)], [
-            Output(pub_key=self.satoshi_pub,
-                   amount=900)])
-        with self.assertRaises(ValueError):
-            self.c.process_tx(satoshi2)
-    
     def test_amounts(self):
         # Check against amount exceeds
-        clemens_priv, clemens_pub = new_key()
-        s_to_c = TransactionBuilder([
-            Input(tx_id=self.genesis_tx.tx_id(),
-                  index=0,
-                  signature=self.satoshi_priv)], [
-            Output(pub_key=self.satoshi_pub,
-                   amount=900),
-            Output(pub_key=clemens_pub,
-                   amount=1000)])
+        s_to_c = Transaction(
+            [Input(tx_id=self.genesis_tx.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=900),
+             Output(pub_key=self.clemens_pub, amount=1000)])
         with self.assertRaises(ValueError):
-          self.c.process_tx(s_to_c)
+          self.c.process_tx(s_to_c, s_to_c.make_witness([self.satoshi_priv]))
 
     def test_wrong_sigs(self):
         # Check against wrong signatures
-        clemens_priv, clemens_pub = new_key()
-        s_to_c = TransactionBuilder([
-            Input(tx_id=self.genesis_tx.tx_id(),
-                  index=0,
-                  signature=clemens_priv)], [
-            Output(pub_key=self.satoshi_pub,
-                   amount=900),
-            Output(pub_key=clemens_pub,
-                   amount=100)])
-        with self.assertRaises(ValueError):
-            self.c.process_tx(s_to_c)
+        # Sign with clemens' key instead of satoshi's.
+        s_to_c = Transaction(
+            [Input(tx_id=self.genesis_tx.tx_id(), index=0)],
+            [Output(pub_key=self.satoshi_pub, amount=900),
+             Output(pub_key=self.clemens_pub, amount=100)])
+        with self.assertRaises(BadSignatureError):
+            self.c.process_tx(s_to_c, s_to_c.make_witness([self.clemens_priv]))
 
 if __name__ == '__main__':
     unittest.main()
