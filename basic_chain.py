463b0dd8722614a15f093fc4c1af823153994b05
Clemens Fruhwirth <clemens@endorphin.org>
2019-02-07 14:48:56 +0100

    Remove obsolete code, transition to utxo tracking fully and replace Output class.

--- basic_chain.py
+++ basic_chain.py
@@ -3,6 +3,7 @@
 
 import hashlib
 import ed25519
+import collections
 
 class Input(object):
     def __init__(self, tx_id, index, signature):
@@ -14,15 +15,7 @@ class Input(object):
         # The signature is not part of the representation
         return "I(%s, %s)" % (self.tx_id, self.index)
 
-class Output(object):
-    def __init__(self, pub_key, amount):
-        self.pub_key = pub_key
-        self.amount = amount
-        self.spent = False
-        
-    def __repr__(self):
-        # The spent attribute is not part of the representation
-        return "O(%s, %s)" % (self.pub_key.to_ascii(encoding="hex"), self.amount)
+Output = collections.namedtuple("Output", ["pub_key", "amount"])
 
 class Transaction(object):
     def  __init__(self, inputs, outputs):
@@ -48,7 +41,6 @@ class TransactionBuilder(Transaction):
 class Chain(object):
     def __init__(self, genesis):
         # Do not verify the genesis transaction
-        self.txs = { genesis.tx_id(): genesis }
         self.utxo = {}
         self.add_utxo(genesis)
 
@@ -63,10 +55,6 @@ class Chain(object):
         tx_id = tx.tx_id()
         effects = []
         for i in tx.inputs:
-#            os = self.txs[i.tx_id].outputs
-#            if not i.index < len(os):
-#                raise ValueError("Input has invalid reference %s" % str(i))
-#            o = os[i.index]
             try:
                 o = self.utxo[(i.tx_id, i.index)]
             except KeyError:
@@ -76,8 +64,6 @@ class Chain(object):
                 o.pub_key.verify(i.signature, tx_id)
             except ed25519.BadSignatureError:
                 raise ValueError("Invalid signature on input %s" % str(i))
-#            if o.spent:
-#                raise ValueError("Input is already spent %s" % str(i))
             def remove_utxo():
                 del self.utxo[(i.tx_id, i.index)]
             effects += [remove_utxo]
@@ -88,11 +74,7 @@ class Chain(object):
             raise ValueError("output amounts %d exceed input amounts %d", o_amount, i_amount)
 
         # All checks passed. Modify our state
-        for i in tx.inputs:
-            o = self.txs[i.tx_id].outputs[i.index]
-#            o.spent = True
         [e() for e in effects]
-        self.txs[tx_id] = tx
         self.add_utxo(tx)
 
 def new_key():
